9013603e68462213b4713693ee86d00abceddad2
Stiliyan Goranov <stiliyan.goranov@opencode.com>
2020-06-23 18:00:03 +0300

    Create unit test for adding a dancer to round dance. Currently fails.

--- src/chorb_round_dance.cpp
+++ src/chorb_round_dance.cpp
@@ -3,3 +3,9 @@
 const std::list<ChorbDancer>& ChorbRoundDance::getDancers() const {
   return dancers;
 }
+
+bool ChorbRoundDance::addDancer(const std::string& newDancer,
+                                const std::string& leftDancer,
+                                const std::string& rightDancer) {
+  return false;
+}
--- utest/chorb_round_dance_utest.cpp
+++ utest/chorb_round_dance_utest.cpp
@@ -29,18 +29,10 @@ SCENARIO("Test building of round dance is correct") {
 
       THEN(
           "All the dancers should have grabbed their left and right partners") {
-        auto it = dancers.begin();
-        REQUIRE(it->hasGrabbedLeftDancer());
-        REQUIRE(it->hasGrabbedRightDancer());
-        ++it;
-        REQUIRE(it->hasGrabbedLeftDancer());
-        REQUIRE(it->hasGrabbedRightDancer());
-        ++it;
-        REQUIRE(it->hasGrabbedLeftDancer());
-        REQUIRE(it->hasGrabbedRightDancer());
-        ++it;
-        REQUIRE(it->hasGrabbedLeftDancer());
-        REQUIRE(it->hasGrabbedRightDancer());
+        for (const auto& dancer : dancers) {
+          REQUIRE(dancer.hasGrabbedLeftDancer());
+          REQUIRE(dancer.hasGrabbedRightDancer());
+        }
       }
 
       THEN("The left and right partners of all dancers should be correct") {
@@ -59,6 +51,77 @@ SCENARIO("Test building of round dance is correct") {
         ++it;
         REQUIRE(it->getLeftDancer()->getNickname() == "dancer3");
         REQUIRE(it->getRightDancer()->getNickname() == "dancer1");
+      }
+    }
+  }
+}
+
+SCENARIO("Adding dancer to built round dance is correct") {
+  GIVEN("A round dance with 4 dancers") {
+    RoundDanceBuilder::UPtr builder =
+        std::make_unique<ChorbRoundDanceBuilder>();
+    builder->addDancer("dancer1");
+    builder->addDancer("dancer2");
+    builder->addDancer("dancer3");
+    builder->addDancer("dancer4");
+    ChorbRoundDance dance = builder->getRoundDance();
+
+    WHEN("We add a new dancer between the first and the second dancers") {
+      dance.addDancer("dancer1.5", "dancer1", "dancer2");
+
+      THEN("The dancers should have correct nicknames") {
+        auto it = dance.getDancers().begin();
+        REQUIRE(it->getNickname() == "dancer1");
+        ++it;
+        REQUIRE(it->getNickname() == "dancer1.5");
+        ++it;
+        REQUIRE(it->getNickname() == "dancer2");
+        ++it;
+        REQUIRE(it->getNickname() == "dancer3");
+        ++it;
+        REQUIRE(it->getNickname() == "dancer4");
+        ++it;
+        REQUIRE(it == dance.getDancers().end());
+      }
+
+      THEN("The left and right partners of all dancers should be correct") {
+        // We should have: d1 d1.5 d2 d3 d4
+        auto it = dance.getDancers().begin();
+        REQUIRE(it->getLeftDancer()->getNickname() == "dancer4");
+        REQUIRE(it->getRightDancer()->getNickname() == "dancer1.5");
+
+        ++it;
+        REQUIRE(it->getLeftDancer()->getNickname() == "dancer1");
+        REQUIRE(it->getRightDancer()->getNickname() == "dancer2");
+
+        ++it;
+        REQUIRE(it->getLeftDancer()->getNickname() == "dancer1.5");
+        REQUIRE(it->getRightDancer()->getNickname() == "dancer3");
+
+        ++it;
+        REQUIRE(it->getLeftDancer()->getNickname() == "dancer2");
+        REQUIRE(it->getRightDancer()->getNickname() == "dancer4");
+
+        ++it;
+        REQUIRE(it->getLeftDancer()->getNickname() == "dancer3");
+        REQUIRE(it->getRightDancer()->getNickname() == "dancer1");
+      }
+
+      THEN(
+          "All the dancers should have grabbed their left and right partners") {
+        for (const auto& dancer : dance.getDancers()) {
+          REQUIRE(dancer.hasGrabbedLeftDancer());
+          REQUIRE(dancer.hasGrabbedRightDancer());
+        }
+      }
+    }
+
+    WHEN("We try to add a dancer between the second and the fourth dancers") {
+      THEN(
+          "The operation should fail because the second and fourth dancers are "
+          "not neighbors") {
+        REQUIRE(dance.addDancer("SOME_DANCER_NAME", "dancer2", "dancer4") ==
+                false);
       }
     }
   }
